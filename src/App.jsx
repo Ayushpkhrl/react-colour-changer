c0d7522c1274edda32736a9764b7b0b717504800
Ayushpkhrl <pokhrelayush8@gmail.com>
2025-06-19 23:45:33 +0545

    Describe your changes here

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import InputCo from './assets/components/InputCo'
+
+function App() {
+
+
+   return (
+    <main className="app-container">
+      <h1>Hello</h1>
+    <InputCo/>
+    </main>
+  )
+}
+
+export default App
